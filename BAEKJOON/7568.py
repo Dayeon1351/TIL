6ae27643f1fc42959748d26d21f631d02c82004c
Dayeon Kim <43941366+Dayeon1351@users.noreply.github.com>
2021-11-30 12:20:11 +0900

    Update 7568.py

--- BAEKJOON/7568.py
+++ BAEKJOON/7568.py
@@ -5,11 +5,10 @@ rank = []
 for i in range(len(people)):
     cnt = 1
     for j in range(len(people)):
-        if i != j:
-            x,y = people[i][0],people[j][0]
-            p,q  = people[i][1],people[j][1]
-            if x < y and p < q:
-                cnt += 1
+        x,y = people[i][0],people[j][0]
+        p,q  = people[i][1],people[j][1]
+        if x < y and p < q:
+            cnt += 1
     
     rank.append(str(cnt))
 print(" ".join(rank))
