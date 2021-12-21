2fd92f3f7d16af5742bbde6b8a4240a8622c1d78
Dayeon Kim <43941366+Dayeon1351@users.noreply.github.com>
2021-12-21 19:54:14 +0900

    Update 15651.py

--- BAEKJOON/15651.py
+++ BAEKJOON/15651.py
@@ -1,4 +1,17 @@
-from itertools import product
+# from itertools import product
+# n,m = map(int,input().split())
+# for i in list(product(range(1,n+1),repeat = m)):
+#     print(' '.join(map(str,i)))
+
 n,m = map(int,input().split())
-for i in list(product(range(1,n+1),repeat = m)):
-    print(' '.join(map(str,i)))
+num = [0] * m
+
+def printNum(m,idx):
+    if m == 0:
+        print(" ".join(num))
+    else:
+        for i in range(1,n+1):
+            num[idx] = str(i)
+            printNum(m-1,idx+1)
+
+printNum(m,0)
