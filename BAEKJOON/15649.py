f117e6684d73733fc1c89382a8ca296935773a39
Dayeon Kim <43941366+Dayeon1351@users.noreply.github.com>
2022-01-01 11:32:03 +0900

    Update 15649.py

--- BAEKJOON/15649.py
+++ BAEKJOON/15649.py
@@ -1,4 +1,22 @@
-from itertools import permutations 
+# from itertools import permutations 
+# n,m = map(int,input().split())
+# for i in list(permutations(range(1,n+1),m)):
+#     print(' '.join(map(str,i)))
+
 n,m = map(int,input().split())
-for i in list(permutations(range(1,n+1),m)):
-    print(' '.join(map(str,i)))
+num = []
+visited = [False] * n
+
+def printNum(m):
+    if m == 0:
+        print(" ".join(num))
+    else:
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                num.append(str(i+1))    
+                printNum(m-1)
+                num.pop()
+                visited[i] = False
+
+printNum(m)
