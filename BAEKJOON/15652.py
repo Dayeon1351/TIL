7fc1d3e06ab880b7069c60052edaabc0c7c95c2c
Dayeon Kim <43941366+Dayeon1351@users.noreply.github.com>
2022-01-01 11:31:20 +0900

    Update 15652.py

--- BAEKJOON/15652.py
+++ BAEKJOON/15652.py
@@ -1,5 +1,19 @@
-from itertools import combinations_with_replacement
-n,m = map(int,input().split())
+# from itertools import combinations_with_replacement
+# n,m = map(int,input().split())
 
-for i in list(combinations_with_replacement(range(1,n+1),m)):
-    print(' '.join(map(str,i)))
+# for i in list(combinations_with_replacement(range(1,n+1),m)):
+#     print(' '.join(map(str,i)))
+
+n,m = map(int,input().split())
+num = []
+
+def printNum(m,idx):
+    if m == 0:
+        print(" ".join(num))
+    else:
+        for i in range(idx,n):
+            num.append(str(i+1))    
+            printNum(m-1,i)
+            num.pop()    
+
+printNum(m,0)
