a42bde756283cb6b28ab32efc9bc5cdc4f6e1920
Dayeon Kim <43941366+Dayeon1351@users.noreply.github.com>
2022-01-01 11:31:41 +0900

    Update 15650.py

--- BAEKJOON/15650.py
+++ BAEKJOON/15650.py
@@ -1,4 +1,22 @@
-from itertools import combinations
+# from itertools import combinations
+# n,m = map(int,input().split())
+# for i in list(combinations(range(1,n+1),m)):
+#     print(' '.join(map(str,i)))
+
 n,m = map(int,input().split())
-for i in list(combinations(range(1,n+1),m)):
-    print(' '.join(map(str,i)))
+num = []
+visited = [False] * n
+
+def printNum(m,idx):
+    if m == 0:
+        print(" ".join(num))
+    else:
+        for i in range(idx,n):
+            if not visited[i]:
+                visited[i] = True
+                num.append(str(i+1))    
+                printNum(m-1,i)
+                num.pop()
+                visited[i] = False
+
+printNum(m,0)
